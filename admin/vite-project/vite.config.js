f766604714a6ef903ff056efda8bfc788c2668a5
Nikita <nikitabajiya18@gmail.com>
2024-07-25 22:57:26 +0530

    Update vite.config.js

--- admin/vite-project/vite.config.js
+++ admin/vite-project/vite.config.js
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/Full_Stack_Ecommerce/admin/vite-project/', 
 })
